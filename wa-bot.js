67e51f09d716d25adcb5d7d9d4d2cf2b5958abc0
pixelupmrk <pixelupmrkt@gmail.com>
2025-09-03 12:25:03 -0300

    Delete wa-bot.js

--- wa-bot.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Código do wa-bot aqui
-    console.log('wa-bot script carregado com sucesso!');
-    // Adicione aqui qualquer inicialização específica do wa-bot
-});
