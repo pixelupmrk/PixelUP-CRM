7cd033ca893f7744742b0f82a12bfee1ee42b0b4
pixelupmrk <pixelupmrkt@gmail.com>
2025-09-03 11:24:44 -0300

    Create wa-bot.js

---
+++ wa-bot.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Código do wa-bot aqui
+    console.log('wa-bot script carregado com sucesso!');
+    // Adicione aqui qualquer inicialização específica do wa-bot
+});
